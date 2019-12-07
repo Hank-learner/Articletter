b7e2cb4a850faac93de5a8db4f183804f8ad45d5
Hank-learner <jerryrocker6@gmail.com>
2019-12-08 02:12:27 +0530

    added base pages

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            "id": 1,
+            "title": "article1",
+            "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro possimus quod quia aperiam quisquam sapiente, architecto itaque! Amet nemo voluptatum numquam iste eveniet excepturi voluptates, cumque dolorem eligendi, ipsa pariatur. Magni magnam temporibus accusantium tenetur. Dolor, fuga. Sed nemo veritatis, aperiam fuga adipisci quos vitae. Harum aperiam doloribus quaerat blanditiis dolorum rem aliquid hic autem quo delectus repellendus, neque placeat, error explicabo deleniti nisi suscipit? Odit mollitia eos deserunt saepe, minima pariatur vitae non dolores",
+            "author": "bell ding",
+            "create_date": "04-25-2019",
+        },
+                {
+            "id": 2,
+            "title": "article2",
+            "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro possimus quod quia aperiam quisquam sapiente, architecto itaque! Amet nemo voluptatum numquam iste eveniet excepturi voluptates, cumque dolorem eligendi, ipsa pariatur. Magni magnam temporibus accusantium tenetur. Dolor, fuga. Sed nemo veritatis, aperiam fuga adipisci quos vitae. Harum aperiam doloribus quaerat blanditiis dolorum rem aliquid hic autem quo delectus repellendus, neque placeat, error explicabo deleniti nisi suscipit? Odit mollitia eos deserunt saepe, minima pariatur vitae non dolores",
+            "author": "bell ding",
+            "create_date": "04-25-2019",
+        },
+                {
+            "id": 3,
+            "title": "article3",
+            "body": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro possimus quod quia aperiam quisquam sapiente, architecto itaque! Amet nemo voluptatum numquam iste eveniet excepturi voluptates, cumque dolorem eligendi, ipsa pariatur. Magni magnam temporibus accusantium tenetur. Dolor, fuga. Sed nemo veritatis, aperiam fuga adipisci quos vitae. Harum aperiam doloribus quaerat blanditiis dolorum rem aliquid hic autem quo delectus repellendus, neque placeat, error explicabo deleniti nisi suscipit? Odit mollitia eos deserunt saepe, minima pariatur vitae non dolores",
+            "author": "bell ding",
+            "create_date": "04-25-2019",
+        }
+    ]
+    return articles
